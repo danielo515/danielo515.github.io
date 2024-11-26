4bf9114d3fa64d78f554597b3cb7ec44e1c5754e
Danielo Rodriguez <rdanielo@gmail.com>
2024-11-26 13:31:38 +0100

    feat: point to my CV on max experience

--- astro/tailwind.config.mjs
+++ astro/tailwind.config.mjs
@@ -4,7 +4,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-in": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(1rem)" },
+        },
+      },
+      animation: {
+        "bounce-right": "slide-in 0.5s ease-in-out infinite alternate",
+      },
+    },
   },
   plugins: [
     function ({ addUtilities, theme }) {
