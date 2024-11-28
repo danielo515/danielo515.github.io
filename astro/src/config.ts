87bd83b52a2d5d1d27e077e33c57afce4066b9ad
Danielo Rodriguez <rdanielo@gmail.com>
2024-11-28 13:54:02 +0100

    chore: extract config and fix layout a bit

---
+++ astro/src/config.ts
@@ -0,0 +1,26 @@
+import type { links } from "./links";
+
+export const config: links = {
+  footerLinks: [
+    { label: "Blog", href: "/blog" },
+    { label: "CV", href: "/cv" },
+    { label: "Projects", href: "/#projects" },
+  ],
+  navItems: [
+    {
+      title: "Experience",
+      label: "experience",
+      href: "/#experience",
+    },
+    {
+      title: "Projects",
+      label: "projects",
+      href: "/#projects",
+    },
+    {
+      title: "About me",
+      label: "about-me",
+      href: "/#about-me",
+    },
+  ],
+};
